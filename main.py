eefa0c74ebb40459f285bb898815bac02aa86f9c
ryanacarter <ryanacarter@comcast.net>
2014-02-25 19:33:25 -0500

    Fixed SQL Statement, the program can now communicate with the database and verify a user to allow them to login. Also cleaned up the rest of the indentation errors.

--- main.py
+++ main.py
@@ -16,39 +16,43 @@ Config.set('graphics', 'height', '1136')
 
 class LoginPage(FloatLayout):
 
-        def check_login_info(self, username, password):
+        def check_login_info(self, uname, pword):
                 
                 # create the database connection
-                db = mdb.connect("localhost","assassins","checkout","assassins");
+                db = mdb.connect("localhost", "assassins", "checkout", "assassins");
 
                 # set the cursor to extract the data
                 cur = db.cursor()
                 
                 # the statement to run in the database.
-                sql = "SELECT * FROM users WHERE username=%s" % (username)
+                sql = "SELECT * FROM users WHERE username = \"%s\"" % (uname)
                 
-                #try the connection
+                # try the connection
                 try:
                         cur.execute(sql)
         
                         results = cur.fetchall()
-                           
-                        uid = row[0]
-                        sqlusername = row[1]
-                        sqlpassword = row[2]
+                        for row in results:
+                            uid = row[0]
+                            username = row[1]
+                            password = row[2]
 
-                        print "sqlusername = %s, sqlpassword= %s" % (sqlusername, sqlpassword)
+                        if uname == username:
+                            if pword == password:
+                                return(1)
+                            else:
+                                return(3)
+                        else:
+                            return(2)
 
                 except:
                         db.close()
-                        return 0
-
-
+                        return(0)
         
         # Varifies the user login information
-                        def login_button_function(self):
+        def login_button_function(self):
 
-                                #gets the data from the text inputs on the login page
+                                # gets the data from the text inputs on the login page
                                 username = self.ids['username']
                                 password = self.ids['password']
                                 invalid = self.ids['invalid_login']
@@ -58,23 +62,22 @@ class LoginPage(FloatLayout):
                                         invalid.text = ""
                                         if len(password.text) > 0:
                                                 varify = self.check_login_info(username.text, password.text);
-                                                #if(varify == 0):
-                                                #    invalid.text = "Connection not Avalible"
-                                                #elif(varify == 1):
-                                                #   invalid.text = "Logged IN"
-                                                #elif(varify == 2):
-                                                #       invalid.text = "Invalid Username"
-                                                #       username.text = ""
-                                                #elif(varify == 3)
-                                                #       invalid.text = "Invalid Password"
-                                                #       password.text = ""
-                                                #else:
-                                                #    invalid.text = "Unable to login"
+                                                if(varify == 0):
+                                                    invalid.text = "Connection not Avalible"
+                                                elif(varify == 1):
+                                                    invalid.text = "Logged IN"
+                                                elif(varify == 2):
+                                                    invalid.text = "Invalid Username"
+                                                    username.text = ""
+                                                elif(varify == 3):
+                                                    invalid.text = "Invalid Password"
+                                                    password.text = ""
+                                                else:
+                                                    invalid.text = "Unable to login"
                                         else:
                                                 invalid.text = "Please enter a password"
                                 else:
                                         invalid.text = "Please enter a username"
-                                print self.check_login_info(username.text, password.text)
 
 class AssassinsApp(App):
         def build(self):
--- test_sql.py
+++ test_sql.py
@@ -4,33 +4,32 @@ import sys
 uname = "rcarter"
 pword = ""
 
-db = mdb.connect("localhost","assassins","checkout","assassins");
+db = mdb.connect("localhost", "assassins", "checkout", "assassins");
 
 cur = db.cursor()
-    
-sql = "SELECT * FROM users WHERE uid = 1"
+	
+sql = "SELECT * FROM users WHERE username = \"%s\"" % (uname)
 
 try:
-    cur.execute(sql)
-    
-    results = cur.fetchall()
-    for row in results:
-        uid = row[0]
-        username = row[1]
-        password = row[2]
-        
-        print "uid=%d, username=%s, password=%s" % \
-             (uid, username, password )
+	cur.execute(sql)
+	
+	results = cur.fetchall()
+	for row in results:
+		uid = row[0]
+		username = row[1]
+		password = row[2]
+		
+		print "uid=%d, username=%s, password=%s" % \
+			 (uid, username, password)
 
 	if uname == username:
 		print "Username"
 	if pword == password:
 		print "password"
-    
+	
 except:
   
-    print "Error unable to fetch data"
+	print "Error unable to fetch data"
 
 db.close()
-
 
