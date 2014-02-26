3c5d31d5546f05900499423ad0590bbfab52808c
ryanacarter <ryanacarter@comcast.net>
2014-02-25 22:36:17 -0500

    started screen transitions, was not able to get the full effect but was able to set up the transition. The main screen will not fully disaper.

--- main.py
+++ main.py
@@ -8,13 +8,21 @@ from kivy.config import Config
 from kivy.uix.widget import Widget
 import pprint
 import MySQLdb as mdb
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
 
+Builder.load_file('assassins.kv')
 
 # Set the default size of the window to the resolution of an iPhone5 Screen.
 Config.set('graphics', 'width', '640')
 Config.set('graphics', 'height', '1136')
 
-class LoginPage(FloatLayout):
+class NewUserScreen(Screen, FloatLayout):
+
+        def createUser(self):
+                return()
+
+class LoginScreen(Screen, FloatLayout):
 
         def check_login_info(self, uname, pword):
                 
@@ -63,25 +71,30 @@ class LoginPage(FloatLayout):
                                         if len(password.text) > 0:
                                                 varify = self.check_login_info(username.text, password.text);
                                                 if(varify == 0):
-                                                    invalid.text = "Connection not Avalible"
+                                                        invalid.text = "Connection not Avalible"
                                                 elif(varify == 1):
-                                                    invalid.text = "Logged IN"
+                                                        sm.current = 'NewUser'
                                                 elif(varify == 2):
-                                                    invalid.text = "Invalid Username"
-                                                    username.text = ""
+                                                        invalid.text = "Invalid Username"
+                                                        username.text = ""
                                                 elif(varify == 3):
-                                                    invalid.text = "Invalid Password"
-                                                    password.text = ""
+                                                        invalid.text = "Invalid Password"
+                                                        password.text = ""
                                                 else:
-                                                    invalid.text = "Unable to login"
+                                                        invalid.text = "Unable to login"
                                         else:
                                                 invalid.text = "Please enter a password"
                                 else:
                                         invalid.text = "Please enter a username"
 
+sm = ScreenManager(transition=NoTransition())
+sm.add_widget(LoginScreen(name='Login'))
+sm.add_widget(NewUserScreen(name='NewUser'))
+
 class AssassinsApp(App):
         def build(self):
-                return LoginPage()
+                return sm
+        
 
 if __name__ == '__main__':
         AssassinsApp().run()
